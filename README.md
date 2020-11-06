2d95038d5d5d34ad31db2ae06bb101d4ea419572
daniel-orlov <daniel.orlov@ctemplar.com>
2020-11-06 14:58:42 +0300

    adding rough structure updating README.md updating .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # parser_plugin_file.d
-Action plugin for file.d pipeline that parses nginx logs
+###### Action plugin for file.d pipeline that parses nginx logs
+
+Existing action plugins structure comparison: [link to Gitmind map][link]
+
+
+
+[link]: https://gitmind.com/app/doc/5a01110255
---
+++ main.go
@@ -0,0 +1,36 @@
+package parser_plugin_file_d
+
+import (
+	"github.com/ozonru/file.d/fd"
+	"github.com/ozonru/file.d/pipeline"
+)
+
+type Plugin struct {
+	config *Config
+}
+
+type Config struct {
+}
+
+func init() {
+	fd.DefaultPluginRegistry.RegisterAction(&pipeline.PluginStaticInfo{
+		Type:    "parse_nginx_logs",
+		Factory: factory,
+	})
+}
+
+func factory() (pipeline.AnyPlugin, pipeline.AnyConfig) {
+	return &Plugin{}, &Config{}
+}
+
+func (p *Plugin) Start(_ pipeline.AnyConfig, _ *pipeline.ActionPluginParams) {
+
+}
+
+func (p *Plugin) Stop() {
+
+}
+
+func (p *Plugin) Do(_ *pipeline.Event) pipeline.ActionResult {
+	return pipeline.ActionDiscard //currently just discards every action it encounters
+}
